9a77258c3e48dc91abd075bc577d2b7b722c81d0
Pankaj Chouhan <pankajchouhan80@gmail.com>
2023-08-29 15:41:18 +0530

    Update Dummy_DataBase.sql

--- MySQL/Dummy_DataBase.sql
+++ MySQL/Dummy_DataBase.sql
@@ -1,19 +1,3 @@
-/*
-*********************************************************************
-http://www.mysqltutorial.org
-*********************************************************************
-Name: MySQL Sample Database classicmodels
-Link: http://www.mysqltutorial.org/mysql-sample-database.aspx
-Version 3.1
-+ changed data type from DOUBLE to DECIMAL for amount columns
-Version 3.0
-+ changed DATETIME to DATE for some colunmns
-Version 2.0
-+ changed table type from MyISAM to InnoDB
-+ added foreign keys for all tables 
-*********************************************************************
-*/
-
 
 /*!40101 SET NAMES utf8 */;
 
