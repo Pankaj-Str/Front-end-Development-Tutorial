f27b887d7b84a56e72d648219bbd40885329e22b
Pankaj Chouhan <pankajchouhan80@gmail.com>
2024-06-20 07:40:35 +0530

    update data --

--- JavaScript/01 Day  Basic/Basic.js
+++
@@ -1,5 +0,0 @@
-// print text 
-document.write("Welcome to @p4n.in") 
-// print text into console
-console.log("codewithpankaj.com")
-
--- UI-Design/Animated Glowing Radial Menu/js/script.js
+++
@@ -1,9 +0,0 @@
-
-
-let toggle = document.querySelector('.toggle')
-let menu = document.querySelector('.menu')
-
-toggle.onclick = ()=>{
-    menu.classList.toggle('active')
-}
-
